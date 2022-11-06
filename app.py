73010ad95eab3acae9f9a18796c16a2ec1ce9d84
derick-amontagna <derick.montagna@3778.care>
2022-11-06 09:50:39 -0300

    Apply black

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ from dash_bootstrap_components.themes import FLATLY as theme
 app = Dash(
     __name__,
     external_stylesheets=[theme],
-    meta_tags=[{"name": "viewport", "content": "width=device-width",
-     "initial-scale":"1.0", 
-     "max-scale":"1.2",
-     "minimum-scale":"0.5"}],
+    meta_tags=[
+        {
+            "name": "viewport",
+            "content": "width=device-width",
+            "initial-scale": "1.0",
+            "max-scale": "1.2",
+            "minimum-scale": "0.5",
+        }
+    ],
 )
 app.title = "Derick's Portfolio Website"
 server = app.server
