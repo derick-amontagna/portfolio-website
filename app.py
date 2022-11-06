d419a0fc8658293c287a55bd670b9a8e0e5f713c
derick-amontagna <derick.montagna@3778.care>
2022-11-06 09:49:50 -0300

    Fix view in mobile devices

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from dash_bootstrap_components.themes import FLATLY as theme
 app = Dash(
     __name__,
     external_stylesheets=[theme],
-    meta_tags=[{"name": "viewport", "content": "width=device-width"}],
+    meta_tags=[{"name": "viewport", "content": "width=device-width",
+     "initial-scale":"1.0", 
+     "max-scale":"1.2",
+     "minimum-scale":"0.5"}],
 )
 app.title = "Derick's Portfolio Website"
 server = app.server
